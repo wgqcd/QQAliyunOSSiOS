c76463a67e1fba78c581277c94376c2c8c3101c0
魏国强 <429172300@qq.com>
2018-10-12 14:40:50 +0800

    no message

--- Example/QQAliyunOSSiOS/QQViewController.m
+++ Example/QQAliyunOSSiOS/QQViewController.m
@@ -22,7 +22,7 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    [QQOSSImageManager enableLog];
+//    [QQOSSImageManager enableLog];
 }
 - (void)imagePickerControllerDidCancel:(UIImagePickerController *)picker{
     [picker dismissViewControllerAnimated:YES completion:nil];
