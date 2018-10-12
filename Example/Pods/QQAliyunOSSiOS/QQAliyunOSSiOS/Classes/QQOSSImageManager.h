97ef5e484306078ec1fc47ec1a4d355a87ad0668
魏国强 <429172300@qq.com>
2018-10-12 14:46:08 +0800

    no message

--- Example/Pods/QQAliyunOSSiOS/QQAliyunOSSiOS/Classes/QQOSSImageManager.h
+++ Example/Pods/QQAliyunOSSiOS/QQAliyunOSSiOS/Classes/QQOSSImageManager.h
@@ -6,7 +6,7 @@
 //
 
 #import <Foundation/Foundation.h>
-@class RACSignal<__covariant ObjectType>;
+#import <ReactiveObjC/ReactiveObjc.h>
 
 NS_ASSUME_NONNULL_BEGIN
 @interface ALiOSSBucket : NSObject
